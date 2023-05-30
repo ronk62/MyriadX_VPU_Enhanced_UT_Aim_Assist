3b016fe09279235106802d2aba9293a2d7b4f79c
Ron King <ronk62@gmail.com>
2023-05-29 22:17:56 -0700

    bug fixes and cleanup

--- pidController.py
+++ pidController.py
@@ -17,30 +17,21 @@ recombine Y and X results into 'target'
 
 class PID:
     def __init__(self):
-        pass
+        self.errorPrevious = 0
+        self.errorSum = 0
 
     def computePidOut(self, Kp=1, Ki=0.1, Kd=0.1, errorCurrent=0, initialize=False):
         if initialize:
             self.errorPrevious = 0
             self.errorSum = 0
-            self.timePrevious = time.time() - 0.000001
         
-        if self.timePrevious == None:
-            self.timePrevious = time.time() - 0.000001
-        
-        # self.dT = time.time() - self.timePrevious
-        self.dT = 0.051
 
-        if self.dT < 0.05:
-            self.dT = 0.05
-
-        # print("self.dT is ------> ", self.dT)
-        # self.errorSum = self.errorSum + (errorCurrent * self.dT)
         self.errorSum = self.errorSum + errorCurrent
-        # self.deltaError = (errorCurrent - self.errorPrevious) / (self.dT + 0.000001)
+        if abs(self.errorSum) > 200:     # clamp the errorSum (integral) at 200
+            self.errorSum = 200 * (abs(self.errorSum) /  self.errorSum)
+        # print("self.errorSum -------------> ", self.errorSum)
         self.deltaError = errorCurrent - self.errorPrevious
         self.pidOut = (Kp * errorCurrent) + (Ki * self.errorSum) + (Kd * self.deltaError)
         self.errorPrevious = errorCurrent
-        self.timePrevious = time.time()
         return self.pidOut
 
