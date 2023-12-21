dbe17892f017c9296bb68b7e4b0eb54f82bfb530
Ron King <ronk62@gmail.com>
2023-12-20 17:09:14 -0800

    change UT99FOV to 75

--- Keyboard_And_Mouse_Controls.py
+++ Keyboard_And_Mouse_Controls.py
@@ -13,8 +13,9 @@ ref. https://www.unknowncheats.me/forum/apex-legends/495561-calculate-mouse-move
 '''
 gameScrnWidth = 1600
 gameScrnHeight = 900
+UT99FOV = 75
 # UT99FOV = 90
-UT99FOV = 120
+# UT99FOV = 120
 UT99sens = 1
 UTfull360 = 16363.0 / UT99sens
 
